7746b42bb5de17e6914fc4fd4d99b4be39a50bc3
plucodev <paololucanodeveloper@gmail.com>
2019-08-04 18:37:30 -0400

    Completed 05

--- exercises/03-Inline-Styles/tests.js
+++ exercises/03-Inline-Styles/tests.js
@@ -27,38 +27,4 @@ describe("The Table tag should contain inline style background: green", function
   });
 });
 
-// describe("The Table tag should contain inline style background:green", function() {
-//   beforeEach(() => {
-//     //here I import the HTML into the document
-//     document.documentElement.innerHTML = html.toString();
-//     document.querySelector(
-//       "head"
-//     ).innerHTML = `<style>${css.toString()}</style>`;
-//   });
-//   afterEach(() => {
-//     jest.resetModules();
-//   });
 
-//   // it("the html code should contain a table tag", function() {
-//   // we can read from the source code
-//   // console.log("HTML: " + html.toString());
-//   // console.log("CSS " + css.toString());
-//   // expect(css.toString().toBeFalsy());
-//   // expect(html.toString().indexOf(`table`)).toBeTruthy();
-
-//   //or use query selector to compare hoy mane scriptags do we have
-//   // const pTags = document.querySelectorAll("p");
-//   // expect(pTags.length).toBe(1);
-//   // });
-//   it("the background should be green", function() {
-
-//     const table = document.querySelector("table");
-//     expect(css.toString().toBeFalsy());
-//     expect(table.style.background === "green").toBeTruthy();
-//     // expect(table.contains("styles"));
-//     // var styles = window.getComputedStyle("table");
-//     // console.log("Styles: " + styles);
-//     // expect(styles["background"]).toBe("green");
-//     // console.log("Styles: " + table);
-//   });
-// });
--- exercises/05-Specificity/tests.js
+++ exercises/05-Specificity/tests.js
@@ -1,54 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
 
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-const css = fs.readFileSync(path.resolve(__dirname, './styles.css'), 'utf8');
+jest.dontMock("fs");
 
-jest.dontMock('fs');
+describe("All the styles should be applied", function() {
+  beforeEach(() => {
+    //here I import the HTML into the document
+    document.documentElement.innerHTML = html.toString();
 
-describe('All the styles should be applied', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-        document.documentElement.innerHTML = html.toString();
+    //apply the styles from the stylesheet if needed
+    document.querySelector(
+      "head"
+    ).innerHTML = `<style>${css.toString()}</style>`;
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
 
-        //apply the styles from the stylesheet if needed
-        document.querySelector('head').innerHTML = `<style>${css.toString()}</style>`;
-    });
-    afterEach(() => { jest.resetModules(); });
-
-    it('the background should be blue', function () {
-
-        // get computed styles of any element you like
-        const body = document.querySelector('body');
-        var styles = window.getComputedStyle(body);
-        expect(styles["background-color"]).toBe("blue");
-    });
-});
-
-
-describe('All the html should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-        document.documentElement.innerHTML = html.toString();
-    });
-    afterEach(() => { jest.resetModules(); });
-
-    it('the html code should contain a p tag', function () {
-
-        // we can read from the source code
-        console.log(html.toString());
-        expect(html.toString().indexOf(`<p`) > -1).toBeTruthy();
-
-        //or use query selector to compare hoy mane scriptags do we have
-        const pTags = document.querySelectorAll("p");
-        expect(pTags.length).toBe(1);
-    });
-
-
-    it('the p tag should have a class "big"', function () {
-
-        //or use query selector to compare hoy mane scriptags do we have
-        const p = document.querySelector("p");
-        expect(p.classList.contains("big")).toBeTruthy();
-    });
+  it("The original code in styles.css should not be deleted", function() {
+    const oldCss = "ulli{background:blue;}#thirditem{background:yellow;}";
+    expect(
+      css
+        .toString()
+        .replace(/\s/g, "")
+        .toLowerCase()
+    ).toContain(oldCss.toLowerCase());
+  });
+  it("The styles.css should match exactly", function() {
+    const newCss =
+      "ulli{background:blue;}#thirditem{background:yellow;}#thirditem{background:green!important;}";
+    expect(
+      css
+        .toString()
+        .replace(/\s/g, "")
+        .toLowerCase()
+    ).toStrictEqual(newCss.toLowerCase());
+  });
 });
