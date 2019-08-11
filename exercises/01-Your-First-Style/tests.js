b1cb679edc1031044b6e8227c432262b9b388d45
plucodev <paololucanodeveloper@gmail.com>
2019-08-11 19:21:30 -0400

    Fixed issues 01

---
+++ exercises/01-Your-First-Style/tests.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+// const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+
+jest.dontMock("fs");
+
+describe("All the styles should be applied", function() {
+  beforeEach(() => {
+    //here I import the HTML into the document
+    document.documentElement.innerHTML = html.toString();
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  it("The background should be blue", function() {
+    const body = document.querySelector("body");
+    var styles = window.getComputedStyle(body);
+    expect(styles["background"]).toBe("blue");
+  });
+  it("The body tag should not contains any inline style", function() {
+    let bodyInlineStyle = document.getElementsByTagName("body");
+    let emptyBodyInlineStyle = {};
+    expect(bodyInlineStyle[0].style._values).toEqual(emptyBodyInlineStyle);
+    // console.log(bodyInlineStyle[0].style._values.background);
+  });
+});
