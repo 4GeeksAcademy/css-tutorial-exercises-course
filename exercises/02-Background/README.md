4c8fe0c88289c05f1c956197d41823a64603764a
plucodev <paololucanodeveloper@gmail.com>
2019-08-11 19:24:42 -0400

    refactoring 02

--- exercises/02-Background/README.md
+++ exercises/02-Background/README.md
@@ -1,15 +1,15 @@
 # `02` Background
 
-The "background" CSS rule allows us to assign and work with the background of any container- the background can be a color or an image. If it is an image you can specify if you want the image to repeat horizontally, vertically, both, or not at all, and you can also specify if you want it to resize and fit the whole container where its being applied, among other properties that can be modified.
+The "background" CSS rule allows us to assign and work with the background of any container. The background can be a color or an image. If it is an image you can specify if you want the image to repeat horizontally, vertically, both, or not at all, and you can also specify if you want it to resize and fit the whole container where its being applied, among other properties that can be modified.
 
 ## 📝 Instructions:
 
-```plan/text
+```plain/text
 1.  Build the exercise.
 2.  Check the Preview.
-3.  Change the background-size to "contain" (check the styles.css tab).
+3.  Change the background-size to 'contain' (check the styles.css tab).
 4.  Build and Preview the exercise again.
-5.  Change the background-repeat to inherit to make it repeat over the x axis and y axis.
+5.  Change the background-repeat to 'inherit' to make it repeat over the x axis and y axis.
 6.  Build and Preview the exercise again.
 ```
 
--- exercises/02-Background/tests.js
+++ exercises/02-Background/tests.js
@@ -1,36 +1,48 @@
-const fs = require("fs");
-const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
-const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+const fs=require("fs");
+const path=require("path");
+const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
 
 jest.dontMock("fs");
 
-describe("All the styles should be applied", function() {
+describe("All the styles should be applied", function () {
   beforeEach(() => {
     //here I import the HTML into the document
-    document.documentElement.innerHTML = html.toString();
+    document.documentElement.innerHTML=html.toString();
 
     //apply the styles from the stylesheet if needed
     document.querySelector(
       "head"
-    ).innerHTML = `<style>${css.toString()}</style>`;
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    const body=document.querySelector("body");
+    var styles=window.getComputedStyle(body);
   });
   afterEach(() => {
     jest.resetModules();
   });
+  it("The body tag should not contains any inline style", function () {
+    let bodyInlineStyle=document.getElementsByTagName("body");
+    let emptyBodyInlineStyle={};
+    expect(bodyInlineStyle[0].style._values).toEqual(emptyBodyInlineStyle);
+    // expect(bodyInlineStyle[0].style._values.background - repeat).toBe(
+    //   undefined
+    // );
 
-  it("the background-size should be 'contain'", function() {
+    console.log(bodyInlineStyle[0].style._values);
+  });
+  it("The Head tag should not includes a Style tag", function () {
+    expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
+  });
+  it("the background-size should be 'contain'", function () {
     // get computed styles of any element you like
-    const body = document.querySelector("table");
-    console.log("Body: " + body);
-    var styles = window.getComputedStyle(body);
+
     expect(styles["background-size"]).toBe("contain");
   });
 
-  it("the background-repeat should be 'no-repeat'", function() {
+  it("the background-repeat should be 'no-repeat'", function () {
     // get computed styles of any element you like
-    const body = document.querySelector("body");
-    var styles = window.getComputedStyle(body);
+    // const body = document.querySelector("body");
+    // var styles = window.getComputedStyle(body);
 
     expect(styles["background-repeat"]).toBe("inherit");
   });
