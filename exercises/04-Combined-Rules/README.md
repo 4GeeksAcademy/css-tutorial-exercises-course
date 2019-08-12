8c3d06c5731cf148e0dc771e1f27a6c21ea3ccb1
plucodev <paololucanodeveloper@gmail.com>
2019-08-11 20:50:09 -0400

    fixed issues 04

--- exercises/04-Combined-Rules/README.md
+++ exercises/04-Combined-Rules/README.md
@@ -18,10 +18,14 @@ border: black 1px solid;
 
 # 📝 Instructions:
 
+```Plain/Text
 1. Combine the 4 padding rules into just one using the "padding:" rule.
-   Combine the all background rules, but the background size, into just one line using the "background:" rule.
+2. Combine the all background rules, but the background size, into just one line using the "background:" rule.
 
-2. **P.S: The background size cannot be combined, the browsers don't support it yet.**
+
+```
+
+P.S: The background size cannot be combined, the browsers don't support it yet.\*\*
 
 # Hint:
 
--- exercises/04-Combined-Rules/tests.js
+++ exercises/04-Combined-Rules/tests.js
@@ -1,35 +1,86 @@
-const fs = require("fs");
-const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
-const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+const fs=require("fs");
+const path=require("path");
+const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
 
 jest.dontMock("fs");
 
-describe("All the styles should be applied", function() {
+describe("All the styles should be applied", function () {
   beforeEach(() => {
     //here I import the HTML into the document
-    document.documentElement.innerHTML = html.toString();
+    document.documentElement.innerHTML=html.toString();
 
     //apply the styles from the stylesheet if needed
-    document.querySelector(
-      "head"
-    ).innerHTML = `<style>${css.toString()}</style>`;
+    // document.querySelector(
+    //   "head"
+    // ).innerHTML = `<style>${css.toString()}</style>`;
   });
   afterEach(() => {
     jest.resetModules();
   });
 
-  it("The styles.css file should be empty", function() {
+  it("The styles.css file should be empty", function () {
     console.log(css);
-    expect(css.toString() === "").toBeTruthy();
+    expect(css.toString()==="").toBeTruthy();
   });
-
-  it("The Padding - Shorthand Property should be applied ", function() {
-    let styles = document.querySelector("head").innerHTML;
-    console.log("Styles :" + styles);
+  it("The Head tag should includes a Style tag", function () {
+    expect(html.toString().indexOf(`<style`)>-1).toBeTruthy();
+  });
+  it("The body tag should not contains any inline style", function () {
+    let bodyInlineStyle=document.getElementsByTagName("style");
+    let emptyBodyInlineStyle={};
+    expect(bodyInlineStyle[0].style._values).toEqual(emptyBodyInlineStyle);
+    console.log("sty: ", bodyInlineStyle[0]);
+  });
+  it("the width should be '50px'", function () {
     // get computed styles of any element you like
-    // const body = document.querySelector('body');
-    // var styles = window.getComputedStyle(body);
-    expect(styles["background-color"]).toBe("blue");
+    const body=document.querySelector(".myBox");
+    var styles=window.getComputedStyle(body);
+    expect(styles["width"]).toBe("50px");
+  });
+  it("the height should be '50px'", function () {
+    // get computed styles of any element you like
+    const body=document.querySelector(".myBox");
+    var styles=window.getComputedStyle(body);
+    expect(styles["height"]).toBe("50px");
+  });
+  // it("the background should be in rgb code rgb(189, 189, 189)", function() {
+  //   // get computed styles of any element you like
+  //   const body = document.querySelector(".myBox");
+  //   var styles = window.getComputedStyle(body);
+  //   expect(styles["background"]).toBe("rgb(189, 189, 189)");
+  // });
+  it("the background-size should be 'contain'", function () {
+    // get computed styles of any element you like
+    const body=document.querySelector(".myBox");
+
+    var styles=window.getComputedStyle(body);
+    console.log("@@@", styles._values)
+    expect(styles["background-size"]).toBe("contain");
+  });
+  // it("the padding-top should be deleted", function () {
+  //   // get computed styles of any element you like
+  //   const body=document.querySelector(".myBox");
+  //   var styles=window.getComputedStyle(body);
+  //   expect(styles["padding-top"]).toBeFalsy();
+  //   expect(styles["padding-bottom"]).toBeFalsy();
+  //   expect(styles["padding-right"]).toBeFalsy();
+  //   expect(styles["padding-left"]).toBeFalsy();
+
+  // });
+  it("the padding should include the shorthand property", function () {
+    // get computed styles of any element you like
+    const body=document.querySelector(".myBox");
+    var styles=window.getComputedStyle(body);
+    expect(styles["padding"]).toBe("10px 190px 50px 30px");
+
+  });
+  it("the background should include the shorthand property ", function () {
+    // get computed styles of any element you like
+    const body=document.querySelector(".myBox");
+    var styles=window.getComputedStyle(body);
+    expect(styles["background"]).toBe(
+      `rgb(189, 189, 189) url(https://assets.breatheco.de/img/funny/scared-baby.jpg) no-repeat 100px`
+    );
   });
 });
