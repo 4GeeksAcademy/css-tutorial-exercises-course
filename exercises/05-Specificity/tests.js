23117fd487356574310a8ee3f585bedb12fd1be3
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-03 19:21:08 +0000

    updated exercise 05 and fixed issues

--- exercises/05-Specificity/tests.js
+++ exercises/05-Specificity/tests.js
@@ -10,37 +10,60 @@ describe("All the styles should be applied", function () {
     //here I import the HTML into the document
     document.documentElement.innerHTML=html.toString();
 
-    //apply the styles from the stylesheet if needed
-    document.querySelector(
-      "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
   });
   afterEach(() => {
     jest.resetModules();
   });
-  it("The Head tag should not includes a Style tag", function () {
+
+
+  it("You should not change the head tag", function () {
+
+    let meta1 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta c")
+    let meta2 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta n")
+    let link = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<link")
+    let title = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<title")
+    expect(meta1).not.toBe(-1)
+    expect(meta2).not.toBe(-1)
+    expect(link).not.toBe(-1)
+    expect(title).not.toBe(-1)
     expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
-  });
 
 
 
-  it("The original code in styles.css should not be deleted", function () {
-    const oldCss="ulli{background:blue;}#thirditem{background:yellow;}";
-    expect(
-      css
-        .toString()
-        .replace(/\s/g, "")
-        .toLowerCase()
-    ).toContain(oldCss.toLowerCase());
+
+
+  })
+  it("You should not delete or edit the existing code", function () {
+
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let cssArray=document.styleSheets[0].cssRules[0].selectorText;
+    // console.log("cssArray:",cssArray)
+    let cssArrayBackground= document.styleSheets[0].cssRules[0].style.background
+    console.log("back:",cssArrayBackground)
+    let thirdItSelector=document.styleSheets[0].cssRules[1].selectorText;
+    let thirdItBackground = document.styleSheets[0].cssRules[1].style.background
+    expect(thirdItSelector).toBe("#thirditem");
+    expect(thirdItBackground).toBe("yellow");
+    expect(cssArray).toBe("ul li");
+    expect(cssArrayBackground).toBe("blue");
+  })
+  it("You should use a more specific rule using the !important annotation ", function () {
+
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let cssArray=document.styleSheets[0].cssRules;
+    // console.log("NEW", cssArray[2].style._importants)
+    let orangeHoverSelector="";
+    for (let i=0; i<cssArray.length; i++) {
+      if (cssArray[i].selectorText==="#thirditem"&&cssArray[i].style._importants.background==="important") {
+        orangeHoverSelector=cssArray[i].style.background;
+      }
+    }
+
+    expect(orangeHoverSelector).toBe("green");
   });
-  it("The styles.css should match exactly", function () {
-    const newCss=
-      "ulli{background:blue;}#thirditem{background:yellow;}#thirditem{background:green!important;}";
-    expect(
-      css
-        .toString()
-        .replace(/\s/g, "")
-        .toLowerCase()
-    ).toStrictEqual(newCss.toLowerCase());
-  });
+
 });
