4c2278251a28cd1223ad778f999fd80a08a477a0
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-03 19:53:00 +0000

    updated exercise 13 and fixed issues

--- exercises/13-Anchor-Like-Button/tests.js
+++ exercises/13-Anchor-Like-Button/tests.js
@@ -2,8 +2,8 @@ const fs=require("fs");
 const path=require("path");
 const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
 const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
-let divTag=null
-let classTagStyles=null
+
+
 jest.dontMock("fs");
 
 
@@ -12,44 +12,66 @@ describe("All the styles should be applied", function () {
   beforeEach(() => {
     //here I import the HTML into the document
     document.documentElement.innerHTML=html.toString();
-    // let classTagStyles = window.getComputedStyle(
-    //   document.querySelector(".orange-btn")
-    // );
-    //apply the styles from the stylesheet if needed
-    document.querySelector(
-      "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    divTag=document.querySelector(".orange-btn");
-    classTagStyles=window.getComputedStyle(divTag);
+
+
   });
   afterEach(() => {
     jest.resetModules();
   });
 
   it("the  padding  should be '10px'", function () {
+    document.querySelector(
+      "head"
+
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let divTag=document.querySelector(".orange-btn");
+    let classTagStyles=window.getComputedStyle(divTag);
 
     expect(classTagStyles["padding"]).toBe("10px");
   });
   it("the  border radius should be '4px'", function () {
+    document.querySelector(
+      "head"
 
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let divTag=document.querySelector(".orange-btn");
+    let classTagStyles=window.getComputedStyle(divTag);
     expect(classTagStyles["border-radius"]).toBe("4px");
   });
   it("the  border should be '1px solid #ffffff;'", function () {
+    document.querySelector(
+      "head"
 
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let divTag=document.querySelector(".orange-btn");
+    let classTagStyles=window.getComputedStyle(divTag);
     expect(classTagStyles["border"]).toBe("1px solid #ffffff");
   });
   it("the  background should be 'rgb(255, 153, 51)'", function () {
+    document.querySelector(
+      "head"
 
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let divTag=document.querySelector(".orange-btn");
+    let classTagStyles=window.getComputedStyle(divTag);
     expect(classTagStyles["background"]).toBe("rgb(255, 153, 51)");
   });
   it("the  underline should to be removed", function () {
+    document.querySelector(
+      "head"
 
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let divTag=document.querySelector(".orange-btn");
+    let classTagStyles=window.getComputedStyle(divTag);
     expect(classTagStyles["text-decoration"]).toBe("none");
   });
-  it("The Mouse hover property should be blue", function () {
+  it("The mouse hover property should be #cc7a00", function () {
     // get computed styles of any element you like
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
     let cssArray=document.styleSheets[0].cssRules;
-    console.log("***", cssArray)
+
     let orangeHoverSelector="";
     for (let i=0; i<cssArray.length; i++) {
       if (cssArray[i].selectorText===".orange-btn:hover") {
@@ -61,10 +83,29 @@ describe("All the styles should be applied", function () {
   });
 
   it("You should be careful the specifity", function () {
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
     let cssArray=document.styleSheets[0].cssRules[0].selectorText;
     expect(cssArray).toBe(".orange-btn");
   }
   )
+it("You should not change the head tag", function () {
 
+    let meta1 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta c")
+    let meta2 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta n")
+    let link = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<link")
+    let title = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<title")
+    expect(meta1).not.toBe(-1)
+    expect(meta2).not.toBe(-1)
+    expect(link).not.toBe(-1)
+    expect(title).not.toBe(-1)
+    expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
+
+
+
+
+
+  })
 
 });
