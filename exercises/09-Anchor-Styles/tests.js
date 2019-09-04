773531df7e58fdfd1f2f3f763d9cf7e720576e2f
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-04 15:35:25 +0000

    updated #09 and fixed issues

--- exercises/09-Anchor-Styles/tests.js
+++ exercises/09-Anchor-Styles/tests.js
@@ -11,18 +11,17 @@ describe("All the styles should be applied", function () {
     document.documentElement.innerHTML=html.toString();
 
     //apply the styles from the stylesheet if needed
-    document.querySelector(
-      "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    cssArray=document.styleSheets[0].cssRules;
+
+
   });
   afterEach(() => {
     jest.resetModules();
   });
-  it("The Head tag should not includes a Style tag", function () {
-    expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
-  });
+
   it("You should not add your rules above the existing code", function () {
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
     let cssArray=document.styleSheets[0].cssRules[0].selectorText;
     expect(cssArray).toBe(".threeDimension");
   })
@@ -37,6 +36,10 @@ describe("All the styles should be applied", function () {
   it("The border-color rule for the 'threeDimension active ' property should match the instruction color", function () {
     // get computed styles of any element you like
     // let cssArray=document.styleSheets[0].cssRules;
+    document.querySelector(
+      "head"
+    ).innerHTML=`<style>${css.toString()}</style>`;
+    let cssArray=document.styleSheets[0].cssRules;
 
     let orangeHoverSelector="";
     for (let i=0; i<cssArray.length; i++) {
@@ -47,6 +50,19 @@ describe("All the styles should be applied", function () {
 
     expect(orangeHoverSelector).toBe("#000 #aaa #aaa #000");
   });
+
+  it("You should not change the head tag", function () {
+
+    let meta1 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta c")
+    let meta2 = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta n")
+    let link = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<link")
+    let title = document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<title")
+    expect(meta1).not.toBe(-1)
+    expect(meta2).not.toBe(-1)
+    expect(link).not.toBe(-1)
+    expect(title).not.toBe(-1)
+    expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
+  })
 });
 
 
