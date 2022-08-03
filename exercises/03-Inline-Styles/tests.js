c6107db9f77cd6e34d78ba5e82072e7b2f07760a
ErnestoXG <ernestox.061@gmail.com>
2022-08-03 03:18:05 +0000

    upgraded tests to mre modern standard

--- exercises/03-Inline-Styles/tests.js
+++ exercises/03-Inline-Styles/tests.js
@@ -1,24 +1,16 @@
-
 const fs = require("fs");
 const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
-
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+document.documentElement.innerHTML = html.toString();
 
 jest.dontMock("fs");
 
-describe("The Table tag should contain inline style background: green", function() {
-  beforeEach(() => {
-    //here I import the HTML into the document
-    document.documentElement.innerHTML = html.toString();
-  });
-  afterEach(() => {
-    jest.resetModules();
-  });
+describe("The Table tag should contain inline style background: green",  ()=>{
 
-  it("You should not change the existing head tag elements", function () {
+  test("You should not change the existing head tag elements",  ()=>{
     let head = document.querySelector('head')
     expect(head).toBeTruthy()
-    
+
     let meta = head.querySelector("meta")
     expect(meta).toBe(null)
 
@@ -26,7 +18,7 @@ describe("The Table tag should contain inline style background: green", function
     expect(title).toBe('03 Inline Styles')
   })
 
-  it("The background should be green", function() {
+  test("The background should be green",  ()=>{
     const table = document.querySelector("table");
     // expect(table.style.background === "green").toBeTruthy();
     var styles = window.getComputedStyle(table);
