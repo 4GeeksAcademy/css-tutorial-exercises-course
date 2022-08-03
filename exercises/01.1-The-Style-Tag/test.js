824949383b16f787faf05745f7ace48187bfac23
ErnestoXG <ernestox.061@gmail.com>
2022-08-03 01:58:19 +0000

    added tests to check if p tag exists and it has proper styles

---
+++ exercises/01.1-The-Style-Tag/test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+document.documentElement.innerHTML = html.toString();
+
+jest.dontMock('fs');
+
+const p = document.querySelector("p");
+
+test("There should be a p tag", ()=>{
+    expect(p).toBeTruthy()
+})
+test("The p tag color should be blue", ()=>{
+    let styles = window.getComputedStyle(p);
+    expect(styles["color"]).toBe("blue");
+});
+test("The p tag should not contain any inline style", ()=>{
+    let emptyBodyInlineStyle = {};
+    expect(p.style._values).toEqual(emptyBodyInlineStyle);
+});
