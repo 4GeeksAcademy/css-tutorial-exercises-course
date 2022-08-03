708ce0dd5633e6880e3f08c2a0ec84b62e393788
ErnestoXG <ernestox.061@gmail.com>
2022-08-03 03:09:19 +0000

    Upgraded test to modern stanndards and made it less repetitive

--- exercises/02.1-Background/tests.js
+++ exercises/02.1-Background/tests.js
@@ -1,67 +1,52 @@
-const fs=require("fs");
-const path=require("path");
-const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
-const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+const fs = require("fs");
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+document.documentElement.innerHTML = html.toString();
 
 jest.dontMock("fs");
 
-describe("All the styles should be applied", function () {
-  beforeEach(() => {
-    //here I import the HTML into the document
-    document.documentElement.innerHTML=html.toString();
-
-    //apply the styles from the stylesheet if needed
-
-
-
-  });
-  afterEach(() => {
-    jest.resetModules();
-  });
-  it("The body tag should not contains any inline style", function () {
+describe("All the styles should be applied", ()=>{
+  const body = document.querySelector("body");
+  const link = document.querySelector("link");
+  const title = document.querySelector('title');
+  
+  test("The body tag should not contains any inline style", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    let bodyInlineStyle=document.getElementsByTagName("body");
-    let emptyBodyInlineStyle={};
-    expect(bodyInlineStyle[0].style._values).toEqual(emptyBodyInlineStyle);
-    // expect(bodyInlineStyle[0].style._values.background - repeat).toBe(
-    //   undefined
-    // );
-
-    console.log(bodyInlineStyle[0].style._values);
+    ).innerHTML = `<style>${css.toString()}</style>`;
+    let emptyBodyInlineStyle = {};
+    expect(body.style._values).toEqual(emptyBodyInlineStyle);
   });
 
-  it("the background-size should be 'contain' without quotes", function () {
+  test("the background-size should be 'contain' without quotes", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
+    ).innerHTML = `<style>${css.toString()}</style>`;
     // get computed styles of any element you like
-    const body=document.querySelector("body");
-    let styles=window.getComputedStyle(body);
+    let styles = window.getComputedStyle(body);
     expect(styles["background-size"]).toBe("contain");
   });
 
-  it("the background-repeat should be 'inherit' without quotes", function () {
+  test("the background-repeat should be 'inherit' without quotes", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
+    ).innerHTML = `<style>${css.toString()}</style>`;
 
-    const body=document.querySelector("body");
-    let styles=window.getComputedStyle(body);
+    let styles = window.getComputedStyle(body);
     expect(styles["background-repeat"]).toBe("inherit");
   });
-  it("You should not change the existing head tag elements", function () {
+  test("You should not change the existing head tag elements", ()=>{
     let head = document.querySelector('head')
     expect(head).toBeTruthy()
-    
+
     let meta = head.querySelector("meta")
     expect(meta).toBe(null)
-    
-    const pathname = new URL(document.querySelector('link').href).pathname
-    expect(pathname).toBe('/styles.css')
 
-    let title = head.querySelector('title').innerHTML
-    expect(title).toBe('02 Background')
+    const href = link.getAttribute("href")
+    expect(href).toBe('./styles.css')
+
+    let titleInner = title.innerHTML
+    expect(titleInner).toBe('02 Background')
   })
 });
