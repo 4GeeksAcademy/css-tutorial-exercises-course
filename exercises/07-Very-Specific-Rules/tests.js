67b25638673e82fb8c220c2b1a34ef256e4b6a84
ErnestoXG <ernestox.061@gmail.com>
2022-08-04 15:30:01 +0000

    fixed minor errors and improved test format

--- exercises/07-Very-Specific-Rules/tests.js
+++ exercises/07-Very-Specific-Rules/tests.js
@@ -1,86 +1,79 @@
-const fs=require("fs");
-const path=require("path");
-const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
-const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const css = fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+document.documentElement.innerHTML = html.toString();
 
-jest.dontMock("fs");
+jest.dontMock('fs');
 
-describe("All the styles should be applied", function () {
-  beforeEach(() => {
-    //here I import the HTML into the document
-    document.documentElement.innerHTML=html.toString();
-    //apply the styles from the stylesheet if needed
-  });
-  afterEach(() => {
-    jest.resetModules();
-  });
-
-
-  it("The ul li color has to be red", function () {
+describe("All the styles should be applied", ()=>{
+  const meta = document.querySelector("meta")
+  const title = document.querySelector('title')
+  const link = document.querySelector('link')
+  
+  test("The ul li color has to be red", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    let cssArray=document.styleSheets[0].cssRules;
-    let orangeHoverSelector="";
-    for (let i=0; i<cssArray.length; i++) {
-      if (cssArray[i].selectorText==="ul li" || cssArray[i].selectorText==="ul > li") {
-        orangeHoverSelector=cssArray[i].style.color;
+    ).innerHTML = `<style>${css.toString()}</style>`;
+    let cssArray = document.styleSheets[0].cssRules;
+    let orangeHoverSelector = "";
+    for (let i = 0; i < cssArray.length; i++) {
+      if (cssArray[i].selectorText === "ul li" || cssArray[i].selectorText === "ul > li") {
+        orangeHoverSelector = cssArray[i].style["color"];
       }
-      expect(orangeHoverSelector).toBe("red");
     }
+    expect(orangeHoverSelector).toBe("red");
   });
-  it("The ul second element background should be green", function () {
+  test("The ul second element background should be green", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    let cssArray=document.styleSheets[0].cssRules;
+    ).innerHTML = `<style>${css.toString()}</style>`;
+    let cssArray = document.styleSheets[0].cssRules;
 
-    let orangeHoverSelector="";
-    for (let i=0; i<cssArray.length; i++) {
-      if (cssArray[i].selectorText==="ol li:nth-child(2)" || cssArray[i].selectorText==="ol > li:nth-child(2)" ) {
-        orangeHoverSelector=cssArray[i].style['background-color'];
+    let orangeHoverSelector = "";
+    for (let i = 0; i < cssArray.length; i++) {
+      if (cssArray[i].selectorText === "ol li:nth-child(2)" || cssArray[i].selectorText === "ol > li:nth-child(2)") {
+        orangeHoverSelector = cssArray[i].style['background-color'];
 
       }
 
-    }expect(orangeHoverSelector).toBe("green");
+    } expect(orangeHoverSelector).toBe("green");
   })
-  it("The odd rows of the table should have yellow background", function () {
+  test("The odd rows of the table should have yellow background", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    let cssArray=document.styleSheets[0].cssRules;
+    ).innerHTML = `<style>${css.toString()}</style>`;
+    let cssArray = document.styleSheets[0].cssRules;
 
-    let orangeHoverSelector="";
-    for (let i=0; i<cssArray.length; i++) {
-      if (cssArray[i].selectorText==="tr:nth-child(odd)") {
-        orangeHoverSelector=cssArray[i].style['background-color'];
+    let orangeHoverSelector = "";
+    for (let i = 0; i < cssArray.length; i++) {
+      if (cssArray[i].selectorText === "tr:nth-child(odd)") {
+        orangeHoverSelector = cssArray[i].style['background'];
 
       }
 
-    }expect(orangeHoverSelector).toBe("yellow");
+    } expect(orangeHoverSelector).toBe("yellow");
   })
 
-  it("Write all your rules above the existing code", function () {
+  test("Write all your rules above the existing code", ()=>{
     document.querySelector(
       "head"
-    ).innerHTML=`<style>${css.toString()}</style>`;
-    let cssBody=document.styleSheets[0].cssRules[3].selectorText;
-    let cssArray=document.styleSheets[0].cssRules[4].selectorText;
+    ).innerHTML = `<style>${css.toString()}</style>`;
+    let cssBody = document.styleSheets[0].cssRules[3].selectorText;
+    let cssArray = document.styleSheets[0].cssRules[4].selectorText;
     expect(cssArray).toBe("ul li,\nol li");
     expect(cssBody).toBe("body");
   }
   )
-  it("You should not change the existing head tag elements", function () {
+  test("You should not change the existing head tag elements", ()=>{
     let head = document.querySelector('head')
     expect(head).toBeTruthy()
-    
-    let meta = head.querySelector("meta")
-    expect(meta).not.toBe(null)
-    
-    const pathname = new URL(document.querySelector('link').href).pathname
-    expect(pathname).toBe('/styles.css')
-    
-    let title = head.querySelector('title')
-    expect(title).not.toBe(null)
+
+    expect(meta).toBeTruthy()
+
+    let href = link.getAttribute('href')
+    expect(href).toBe('./styles.css')
+
+    expect(title).toBeTruthy()
   })
 });
