4a39434ee6fe5d421aef6ad10cc95bb3e9c6e7a2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-04-11 18:02:49 +0200

    Update test.js

--- exercises/04.3-id-Selector/test.js
+++ exercises/04.3-id-Selector/test.js
@@ -8,24 +8,24 @@ jest.dontMock('fs');
 const span = document.querySelector("span");
 const link = document.querySelector("link");
 
-test("There should be a span tag", () => {
+test("There should be a <span> tag", () => {
     expect(span).toBeTruthy()
 });
 
-test("The span tag should have the id 'button1'", () => {
+test("The <span> tag should have the id 'button1'", () => {
     expect(span).toBeTruthy();
     let id = span.id
     expect(id).toBeTruthy();
     expect(id).toBe('button1');
 });
 
-test("The span tag should not contain any inline style", () => {
+test("The <span> tag should not contain any inline style", () => {
     let emptyBodyInlineStyle = {};
     expect(span).toBeTruthy()
     expect(span.style._values).toEqual(emptyBodyInlineStyle);
 });
 
-test("You should not change the existing head tag elements", () => {
+test("You should not change the existing <head> tag elements", () => {
     let head = document.querySelector('head');
     expect(head).toBeTruthy();
 
