d78f767fe2c84dd8d86dee46dd6c008b95ec8667
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-04-11 18:41:25 +0200

    Update tests.js

--- exercises/11-Font-Awesome-Icons/tests.js
+++ exercises/11-Font-Awesome-Icons/tests.js
@@ -10,23 +10,23 @@ let cssArray = null;
 
 describe("All the styles should be applied", ()=>{
   const icons = document.querySelectorAll("i");
-  test("The ul tag should exist", ()=>{
+  test("The <ul> tag should exist", ()=>{
     expect(document.querySelector("ul")).toBeTruthy();
   });
-  test("At least 3 li tags should exist", ()=>{
+  test("At least 3 <li> tags should exist", ()=>{
     expect(document.querySelectorAll("li").length).toBe(3);
   });
-  test("At least 3 i tags should exist", ()=>{
+  test("At least 3 <i> tags should exist", ()=>{
     expect(document.querySelectorAll("i").length).toBe(3);
   });
-  test("The i tags should be inside the li tags", ()=>{
+  test("The <i> tags should be inside the <li> tags", ()=>{
     expect(document.querySelector("li")).toBeTruthy();
     const li = document.querySelectorAll("li");
     for (let i = 0; i < li.length; i++) {
       expect(li[i].innerHTML).toBeTruthy();
     }
   });
-  test('The i tags should have any Font Awesome class starting with "fa" | ex. "fa-solid"', ()=>{
+  test('The <i> tags should have any Font Awesome class starting with "fa" | ex. "fa-solid"', ()=>{
     //or use query selector to compare hoy mane scriptags do we have
     expect(icons).toBeTruthy();
     
