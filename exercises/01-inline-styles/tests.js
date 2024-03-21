f3a1b99d6b84d84230e2566f040e84f2d8ca3eb7
josemoracard <jose.cardinales@gmail.com>
2024-03-21 13:39:44 +0000

    updated test

--- exercises/01-inline-styles/tests.js
+++ exercises/01-inline-styles/tests.js
@@ -1,13 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require('jsdom');
-
-// Read the HTML file
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+document.documentElement.innerHTML = html.toString();
 
-// Set up the DOM environment
-const { window } = new JSDOM(html);
-const { document } = window;
+jest.dontMock('fs');
 
 // Run the tests
 test("There should be a table element", () => {
